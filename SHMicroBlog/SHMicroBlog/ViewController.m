b4d46736ab7385657361d1bb72f3ff9b638c091e
Barry <lizhe_email@163.com>
2016-06-22 03:43:35 +0800

    First commit

---
+++ SHMicroBlog/SHMicroBlog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SHMicroBlog
+//
+//  Created by Li Zhe on 6/21/16.
+//  Copyright © 2016 SH10. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
