5fa51c344b18e9e8c2e9456088b0c33eded3f698
Barry <lizhe_email@163.com>
2016-06-22 18:26:20 +0800

    Give the special path.

--- SHMicroBlog/SHMicroBlog/AppDelegate.m
+++ SHMicroBlog/SHMicroBlog/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"Path = ");
+    NSLog(@"Path = %@ ", [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject]);
     return YES;
 }
 
